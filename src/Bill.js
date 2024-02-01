b7b98e0fe37fba3ad746eed20c58cf862c64fece
Jp Keys <jpmusik29@gmail.com>
2024-02-01 09:24:56 -0500

    Label input connection using htmlFor

--- src/Bill.js
+++ src/Bill.js
@@ -2,8 +2,10 @@
 export default function Bill(){
     return(
         <div >
-            <label>How much was the bill?</label>
-            <input type="text" placeholder="Bill value" />
+            {/* <label>How much was the bill?</label>
+            <input type="text" placeholder="Bill value" /> */}
+            <label htmlFor="input1" >How much was the bill?</label>
+            <input id="input1" name="input1"></input>
         </div>
     );
 }
