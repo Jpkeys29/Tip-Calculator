f16f7667a05c255574a2d0da9c2b818154401dca
Jp Keys <jpmusik29@gmail.com>
2024-02-06 09:42:13 -0500

    Lifting State up for Bill component

--- src/Total.js
+++ src/Total.js
@@ -1,15 +1,21 @@
 import SelectPercent from "./SelectPercent";
 import Button from "./Button";
 import Bill from "./Bill";
+import Output from "./output";
+import { useState } from "react";
 
 export default function Total() {
+    const [cuenta, setCuenta ] = useState("");
     return(
         <div className="statements">
             <Bill />
             <SelectPercent>How did you like the service?</SelectPercent>
             <SelectPercent>How did your friend like the service?</SelectPercent>
+            <Output />
             <div><Button /></div>        
         </div>
     );
 }
+
+
 
