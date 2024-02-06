cad70ee57563f300247b101af8084695c7e9b9d7
Jp Keys <jpmusik29@gmail.com>
2024-02-06 12:20:13 -0500

    Setting up Select Percent State

--- src/SelectPercent.js
+++ src/SelectPercent.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 
-export default function SelectPercent({children}) {
+
+export default function SelectPercent({children, percent, onSelect}) {
     return(
         <div>
             <label>{children}</label>
-            <select>
-                <option>Dissatisfied (0%)</option>
-                <option>It was okay (5%)</option>
-                <option>It was good (10%) </option>
-                <option>Absolutely amazing! (20%)</option>
+            <select value={percent} onChange={e => onSelect(Number(e.target.value))} >
+                <option >Dissatisfied (0%)</option>
+                <option >It was okay (5%)</option>
+                <option >It was good (10%) </option>
+                <option >Absolutely amazing! (20%)</option>
             </select>
         </div>
     );
--- src/Total.js
+++ src/Total.js
@@ -6,12 +6,14 @@ import { useState } from "react";
 
 export default function Total() {
     const [cuenta, setCuenta ] = useState("");
+    const [mypercent, setMyPercent] = useState(0);
+    const [friendpercent, setFriendPercent] = useState(0);
     return(
         <div className="statements">
             <Bill cuenta={cuenta} onSetCuenta={setCuenta} />
-            <SelectPercent>How did you like the service?</SelectPercent>
+            <SelectPercent percent={mypercent} onSelect={setMyPercent} >How did you like the service?</SelectPercent>
             <SelectPercent>How did your friend like the service?</SelectPercent>
-            <Output cuenta={cuenta} />
+            <Output cuenta={cuenta} percent={mypercent}/>
             <div><Button /></div>        
         </div>
     );
